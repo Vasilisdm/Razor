e293c9c5f1779229fde60bb1e375aeb15c5c0319
Vasilisdm <vasilisdmr@gmail.com>
2019-05-30 22:20:20 +0300

    Created controllers folder and added Home controller.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            return View(myProduct);
+        }
+    }
+}
